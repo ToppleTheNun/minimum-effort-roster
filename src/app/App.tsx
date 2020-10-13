95ba42544a77e09cc6b3752f786217bb0a08c5f7
Richard Harrah <topplethenunnery@gmail.com>
2020-10-13 00:17:16 -0400

    remove home page and redirect to builder

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
 
 import PageWrapper from "../components/halfmoon/PageWrapper";
 import ContentWrapper from "../components/halfmoon/ContentWrapper";
 import AppNavbar from "../components/AppNavbar";
 import PlayerBuilderPage from "../features/playerBuilder/PlayerBuilderPage";
 import store from "./store";
-import HomePage from "../features/home/HomePage";
 import CompositionPage from "../features/composition/CompositionPage";
 
 function App() {
@@ -25,7 +24,7 @@ function App() {
                 <CompositionPage />
               </Route>
               <Route path="/">
-                <HomePage />
+                <Redirect to="/builder" />
               </Route>
             </Switch>
           </ContentWrapper>
